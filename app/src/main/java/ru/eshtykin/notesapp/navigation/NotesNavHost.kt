8953cf1cfa2c926907e2e26ee540de8d3bd45393
Shtykin <shtykin@mail.ru>
2022-11-05 10:43:10 +0600

    lesson 1

---
+++ app/src/main/java/ru/eshtykin/notesapp/navigation/NotesNavHost.kt
@@ -0,0 +1,32 @@
+package ru.eshtykin.notesapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.eshtykin.notesapp.screens.Add
+import ru.eshtykin.notesapp.screens.Main
+import ru.eshtykin.notesapp.screens.Note
+import ru.eshtykin.notesapp.screens.Start
+
+sealed class NavRoute (val route: String){
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Note: NavRoute("note_screen")
+}
+
+@Composable
+fun NotesNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = NavRoute.Start.route
+    ){
+        composable(NavRoute.Start.route) { Start(navController = navController) }
+        composable(NavRoute.Main.route) { Main(navController = navController) }
+        composable(NavRoute.Add.route) { Add(navController = navController) }
+        composable(NavRoute.Note.route) { Note(navController = navController) }
+    }
+}
